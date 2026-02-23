20fadd59a2724fa97837354e9b625d41f871e908
Nandita Mahapatra <155948462+NanditaMahapatra@users.noreply.github.com>
2026-02-23 15:21:53 +0530

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,1 @@
-export const data = [{
-    "id" : 1,
-    "listItem" : "Make projects",
-    "isDeleted" : false,
-    "isCompleted" : false
-},
-{
-    "id" : 2,
-    "listItem" : "Die",
-    "isDeleted" : false,
-    "isCompleted" : false
-}]
+export const data = []
